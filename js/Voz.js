01b0f78a55065e9348025bd7cafb0d89fbd2c04a
serome111 <32885503+serome111@users.noreply.github.com>
2018-11-02 12:34:27 -0500

    Voz al inicar la web
    archivo javascrip

--- js/Voz.js
+++ js/Voz.js
@@ -1,53 +1,19 @@
-var synth = window.speechSynthesis;
-
-var inputForm = document.querySelector('form');
-var Text = "¡Hola!. Bienvenido a la web de. 'Sebastián Roa meneses'. ¡Aqui podras encontrar ayuda!"
-var voiceSelect = 'Monica (es-ES)';
-
-var pitch = 1;
-var pitchValue = 1;
-var rate = 0.8;
-var rateValue = 0.8;
 
 
+var msg = new SpeechSynthesisUtterance();
+var voices = window.speechSynthesis.getVoices();
+msg.voice = voices[10]; // Note: some voices don't support altering params
+msg.voiceURI = 'Monica (es-ES)';
+msg.volume = 1; // 0 to 1
+msg.rate = 0.8; // 0.1 to 10
+msg.pitch = 1; //0 to 2
+msg.text = "¡Hola!. Bienvenido a la WEB. de 'Sebastián Roa meneses'. ¡Aqui podras encontrar ayuda!";
+msg.lang = 'es-ES';
 
-var voices = [];
+msg.onend = function(e) {
+  console.log('Finished in ' + event.elapsedTime + ' seconds.');
+};
 
-function populateVoiceList() {
-  voices = synth.getVoices();
+speechSynthesis.speak(msg);
 
-  for(i = 0; i < voices.length ; i++) {
-    var option = document.createElement('option');
-    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-    
-    if(voices[i].default) {
-      option.textContent += ' -- DEFAULT';
-    }
-
-    option.setAttribute('data-lang', voices[i].lang);
-    option.setAttribute('data-name', voices[i].name);
-    voiceSelect.appendChild(option);
-  }
-}
-
-populateVoiceList();
-if (speechSynthesis.onvoiceschanged !== undefined) {
-  speechSynthesis.onvoiceschanged = populateVoiceList;
-}
-
-inputForm.onsubmi = function(event) {
-  event.preventDefault();
-
-  var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-  var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-  for(i = 0; i < voices.length ; i++) {
-    if(voices[i].name === selectedOption) {
-      utterThis.voice = voices[i];
-    }
-  }
-  utterThis.pitch = pitch.value;
-  utterThis.rate = rate.value;
-  synth.speak(utterThis);
-
-  inputTxt.blur();
-}
+//de aqui saque el ejemplo https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
